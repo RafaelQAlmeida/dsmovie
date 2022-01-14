dc382c8fba26d0b05104974b939f0e9c4f06a183
Rafael Quintiliano Queiroz de Almeida <rafaelqalmeida@outlook.com>
2022-01-14 17:09:15 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -17,12 +17,12 @@ public class MovieController {
 
 	@Autowired
 	private MovieService service;
-	
+
 	@GetMapping
 	public Page<MovieDTO> findAll(Pageable pageable) {
 		return service.findAll(pageable);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public MovieDTO findById(@PathVariable Long id) {
 		return service.findById(id);
